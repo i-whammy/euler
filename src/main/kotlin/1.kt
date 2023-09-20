37648272fc6fd39c33123125e00aa0c1bb34522c
i-whammy <yas.iwami@gmail.com>
2023-09-20 10:09:33 +0900

    euler 2

--- src/main/kotlin/1.kt
+++ src/main/kotlin/1.kt
@@ -2,10 +2,10 @@
 
 fun main() {
     val target = (1..999).toList()
-    println(calc(target))
+    println(multiplesOf3And5(target))
 }
 
-fun calc(numbers: List<Int>): Int {
+fun multiplesOf3And5(numbers: List<Int>): Int {
     return numbers.filter { it % 3 == 0 || it % 5 == 0 }
         .reduce { acc, i -> acc + i }
 }
---
+++ src/main/kotlin/2.kt
@@ -0,0 +1,20 @@
+// https://projecteuler.net/problem=1
+
+fun main() {
+    println(evenFibonacciNumbers())
+}
+
+fun evenFibonacciNumbers(): Int {
+    val limit = 4_000_000
+    val evens = mutableListOf(2)
+    var a = 1
+    var b = 2
+    var c = a + b
+    while (c < limit) {
+        if (c % 2 == 0) evens.add(c)
+        a = b
+        b = c
+        c = a + b
+    }
+    return evens.reduce { acc, i -> acc + i }
+}
