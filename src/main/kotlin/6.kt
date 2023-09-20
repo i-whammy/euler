f6e7303d8211601ab89d75b2db6fc231226de8f1
i-whammy <yas.iwami@gmail.com>
2023-09-20 22:22:00 +0900

    euler 6

---
+++ src/main/kotlin/6.kt
@@ -0,0 +1,14 @@
+import java.math.BigDecimal
+
+fun main() {
+    println(getSumSquareUntil(100) - getSquareSumUntil(100))
+}
+
+fun getSquareSumUntil(number: Int): BigDecimal {
+    return (1..number).map { it * it }.reduce { acc, i -> acc + i }.toBigDecimal()
+}
+
+fun getSumSquareUntil(number: Int): BigDecimal {
+    val i = (1 + number) * number / 2
+    return (i * i).toBigDecimal()
+}
