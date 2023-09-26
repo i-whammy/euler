862676f7e4868a099bb9dadb6d8ea4aaca00ce1c
i-whammy <yas.iwami@gmail.com>
2023-09-26 22:02:21 +0900

    euler 30

---
+++ src/main/kotlin/exercises/30.kt
@@ -0,0 +1,14 @@
+package exercises
+
+import kotlin.math.pow
+
+fun main() {
+    println((1..999999).filter { it == sumOfNthPowerOfDigits(it, 5) }.sum())
+    kotlin.system.exitProcess(0)
+}
+
+fun sumOfNthPowerOfDigits(value: Int, n: Int): Int {
+    return value.toString()
+        .map { it.digitToInt().toDouble().pow(n) }
+        .sum().toInt()
+}
