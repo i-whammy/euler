98a71c3a8a763b23ca5c25cabd7aa8a9d2dc1386
i-whammy <yas.iwami@gmail.com>
2023-09-26 21:50:33 +0900

    euler 29

---
+++ src/main/kotlin/exercises/29.kt
@@ -0,0 +1,17 @@
+package exercises
+
+import java.math.BigInteger
+
+fun main() {
+    println(getUniquePowers(2,5,2,5).size)
+    println(getUniquePowers(2,100,2,100).size)
+    kotlin.system.exitProcess(0)
+}
+
+fun getUniquePowers(aFrom: Int, aTo: Int, bFrom: Int, bTo: Int): Set<BigInteger> {
+    return (aFrom..aTo).map { a ->
+        (bFrom..bTo).map { b ->
+            a.toBigDecimal().pow(b).toBigInteger()
+        }
+    }.flatten().toSet()
+}
