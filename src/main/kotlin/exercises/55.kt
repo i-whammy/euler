2935c35f58ab106ac5ff35afd61944f33f573fa2
i-whammy <yas.iwami@gmail.com>
2023-10-15 20:33:41 +0900

    euler 55

---
+++ src/main/kotlin/exercises/55.kt
@@ -0,0 +1,28 @@
+package exercises
+
+import functions.isPalindromic
+import java.math.BigInteger
+
+// https://projecteuler.net/problem=55
+
+fun main() {
+    println(getAllLychrelNumbers(10_000).size)
+    kotlin.system.exitProcess(0)
+}
+
+fun getAllLychrelNumbers(below: Long): List<Long> {
+    return (1..below).filter { it.isLycrel() }
+}
+
+fun Long.isLycrel(): Boolean {
+    var current = this.toBigInteger()
+    var count = 0
+    while (count < 50) {
+        current += current.reversed()
+        if (isPalindromic(current)) return false
+        count++
+    }
+    return true
+}
+
+fun BigInteger.reversed() = this.toString().reversed().toBigInteger()
