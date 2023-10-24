0655598fe52f6bafab823b50220be99ba32f5bcb
i-whammy <yas.iwami@gmail.com>
2023-10-24 23:28:45 +0900

    euler 63

---
+++ src/main/kotlin/exercises/63.kt
@@ -0,0 +1,18 @@
+package exercises
+
+import java.math.BigInteger
+
+fun main() {
+    val answers = mutableListOf<BigInteger>()
+    (1..1000).forEach {
+        var current = BigInteger.valueOf(1)
+        while (current.pow(it).toString().length <= it) {
+            if (current.pow(it).toString().length == it) {
+                answers.add(current.pow(it))
+            }
+            current++
+        }
+    }
+    println(answers.size)
+    kotlin.system.exitProcess(0)
+}
