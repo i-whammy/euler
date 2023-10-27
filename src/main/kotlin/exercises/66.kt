13638e9d14dc9bb9c6b0af2c44407e3932648e17
i-whammy <yas.iwami@gmail.com>
2023-10-27 23:09:49 +0900

    wip

--- src/main/kotlin/exercises/66.kt
+++ src/main/kotlin/exercises/66.kt
@@ -2,35 +2,38 @@ package exercises
 
 import java.math.BigInteger
 import kotlin.math.pow
+import kotlin.math.sqrt
 
-private const val UPPER_LIMIT = 100_000L
-private val squares = (1..31).map { it.toDouble().pow(2) }
+private const val UPPER_LIMIT = 100_000_000_00L
+private val squares = (1..31).map { it.toDouble().pow(2).toLong() }
 
 fun main() {
-    val ds = (1..1000L).filterNot { squares.contains(it.toDouble()) }
-    var max = 1L
-    var dMax = 0L
+    val ds = (1..65L).filterNot { squares.contains(it) }
+    var xMax = 3L
+    var dMax = 2L
     ds.forEach first@{ d ->
-        (1L..UPPER_LIMIT).forEach { x ->
-            var y = BigInteger.ONE
-            while (x.toBigInteger().pow(2) > y.pow(2) * BigInteger.valueOf(d)) {
-                if (areValid(x, y, d)) {
-//                    println("$x^2 - $d * $y^2 = 1")
-                    if (max < x) {
-                        max = x
+        (1..UPPER_LIMIT).forEach { x ->
+//            if (d == 61L) println(x)
+            val y = sqrt((x.toDouble().pow(2) - 1) / d).toLong()
+
+            if (y > 0 &&
+                y.toDouble().pow(2).toLong() * d + 1 == x.toDouble().pow(2).toLong()) {
+                if (areValid(x.toBigInteger(), y.toBigInteger(), d)) {
+                    println("$x^2 - $d * $y^2 = 1")
+                    if (xMax < x) {
+                        xMax = x
                         dMax = d
                     }
                     return@first
                 }
-                y++
             }
         }
-        println(d) // at least when d=61 x might be too big so that x is not determined
+        println("omitted d = $d") // at least when d=61 x might be too big so that x is not determined
     }
     println(dMax)
     kotlin.system.exitProcess(0)
 }
 
-fun areValid(x: Long, y: BigInteger, d: Long): Boolean {
-    return (x.toDouble().pow(2) - d * y.toDouble().pow(2)).toLong() == 1L
+fun areValid(x: BigInteger, y: BigInteger, d: Long): Boolean {
+    return (x.pow(2) - d.toBigInteger() * y.pow(2)) == BigInteger.ONE
 }
