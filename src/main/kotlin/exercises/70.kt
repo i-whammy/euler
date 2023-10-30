6d2a2396f85571580db10dedf2e50795747cee36
i-whammy <yas.iwami@gmail.com>
2023-10-30 22:15:46 +0900

    euler 70

---
+++ src/main/kotlin/exercises/70.kt
@@ -0,0 +1,19 @@
+package exercises
+
+fun main() {
+    val limit = 9_999_999
+    val totients = totientsList(limit)
+    var min = limit.toDouble()
+    var minN = 0
+    totients.forEachIndexed { index, i ->
+        if ((0..1).contains(index) || !arePermutation(index, i)) return@forEachIndexed
+        if (index / i.toDouble() < min) {
+            min = index / i.toDouble()
+            minN = index
+        }
+    }
+    println(minN)
+    kotlin.system.exitProcess(0)
+}
+
+fun arePermutation(a: Int, b: Int): Boolean = a.toString().toList().sorted() == b.toString().toList().sorted()
