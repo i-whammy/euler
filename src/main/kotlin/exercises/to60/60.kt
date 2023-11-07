037577b82a2649e2b462b940b06032f4388b6eb4
i-whammy <yas.iwami@gmail.com>
2023-11-07 22:55:13 +0900

    wip 60

--- src/main/kotlin/exercises/to60/60.kt
+++ src/main/kotlin/exercises/to60/60.kt
@@ -1,68 +1,77 @@
 package exercises.to60
 
 import functions.isPrime
+import kotlin.math.min
 
-private const val upperLimit = 1_000_000L
-private val primes = (3..upperLimit).filter { isPrime(it.toBigInteger()) } // 2 is prime but it will create even numbers so omit it.
-private val suitablePrimes = primes.filter { it.isSetOfTwoPrimes() }
+private const val upperLimit = 100_000
+private val primes =
+    (3..upperLimit).filter { isPrime(it.toBigInteger()) } // 2 is prime, but it will create even numbers so omit it.
 
 fun main() {
-    val min = primes.map { findSum(mutableListOf(it)) }.filter { it != -1L }.min()
-    println(min)
+    println(findMinimumSumOfPrimeSets(4))
     kotlin.system.exitProcess(0)
 }
 
-fun findSum(resultPrimes: MutableList<Long>, targetLength: Int = 3): Long {
-    when (resultPrimes.size) {
-        targetLength + 1 -> {
-            println(resultPrimes)
-            return resultPrimes.sum()
-        }
-
-        else -> {
-            (1..<primes.indexOf(resultPrimes.last())).map { count ->
-                val candidates = mutableListOf<Long>().also { it.addAll(resultPrimes) }
-                candidates.add(primes[primes.indexOf(resultPrimes.last()) + count])
-                candidates
-            }.filterNot { candidate ->
-                val indices = getPermutedIndices(candidate.size)
-                indices.any { (a, b) ->
-                    val canA = candidate[a].toInt()
-                    val canB = candidate[b].toInt()
-                    !suitablePrimes.contains("$canA$canB".toLong())
-                            || !suitablePrimes.contains("$canB$canA".toLong())
-                }
-            }.forEach {
-                findSum(it)
+fun findMinimumSumOfPrimeSets(n: Int): Int {
+    val primeMap = mutableMapOf<Int, List<Int>>()
+    var tmpSum: Int
+    var index = 0
+    while (true) {
+        val current = primes[index]
+        val candidates = (0..<index).map { primes[it] }.filter { areConcatPrimes(current, it) }
+        primeMap[current] = candidates
+        candidates.map { candidate ->
+            val cands = primeMap[candidate]!!
+            cands.filter {
+                cands.any { cand -> primeMap[it]!!.contains(cand) }
             }
         }
+        var count = n - 1
+        val filtered = mutableListOf(current)
+        candidates.reversed().forEachIndexed { i, candidate ->
+            val remainings = candidates.subList(0, i)
+            if (remainings.count { e ->
+                    primeMap[e]?.contains(candidate) == true
+                } == count
+            ) {
+                filtered.add(candidate)
+                count--
+            }
+        }
+        if (filtered.size == n) {
+            tmpSum = filtered.sum()
+            println("$filtered")
+            println("tmpSum = $tmpSum")
+            break
+        }
+        index++
+        continue
     }
-    return -1
+    while (primes[index] < tmpSum) {
+        val current = primes[index]
+        val candidates = (0..<index).map { primes[it] }.filter { areConcatPrimes(current, it) }
+        primeMap[current] = candidates
+        var count = n - 1
+        val filtered = mutableListOf(current)
+        candidates.reversed().forEachIndexed { i, candidate ->
+            val remainings = candidates.subList(0, i)
+            if (remainings.count { e ->
+                    primeMap[e]?.contains(candidate) == true
+                } == count
+            ) {
+                filtered.add(candidate)
+                count--
+            }
+        }
+        if (filtered.size == n) {
+            tmpSum = min(filtered.sum(), tmpSum)
+            println("$filtered")
+            println("tmpSum = $tmpSum")
+        }
+        index++
+        continue
+    }
+    return tmpSum
 }
 
-// nP2 = n * (n-1)
-fun getPermutedIndices(n: Int): List<List<Int>> {
-    if (n <= 1) return emptyList()
-    return (0..<n).flatMap { a ->
-        val remaining = (0..<n).toMutableList()
-        remaining.remove(a)
-        remaining.map { b ->
-            listOf(a, b)
-        }
-    }
-}
-
-fun Long.isSetOfTwoPrimes(): Boolean {
-    val s = this.toString()
-    return when (val length = s.length) {
-        1 -> false
-        else -> (1..<length).any { i ->
-            val first = s.substring(0, i)
-            val second = s.substring(i, length)
-            isPrime(first.toBigInteger())
-                    && second[0] != '0' // prime must not begin with 0
-                    && isPrime(second.toBigInteger())
-                    && isPrime("$second$first".toBigInteger())
-        }
-    }
-}
+fun areConcatPrimes(a: Int, b: Int): Boolean = isPrime("$a$b".toBigInteger()) && isPrime("$b$a".toBigInteger())
