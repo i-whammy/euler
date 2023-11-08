e854afc81519a2b7a68085b314666fd95589b564
i-whammy <yas.iwami@gmail.com>
2023-11-08 12:17:53 +0900

    euler 60

--- src/main/kotlin/exercises/to60/60.kt
+++ src/main/kotlin/exercises/to60/60.kt
@@ -2,74 +2,70 @@ package exercises.to60
 
 import functions.isPrime
 import kotlin.math.min
+import kotlin.math.sign
 
 private const val upperLimit = 100_000
 private val primes =
     (3..upperLimit).filter { isPrime(it.toBigInteger()) } // 2 is prime, but it will create even numbers so omit it.
 
 fun main() {
-    println(findMinimumSumOfPrimeSets(4))
+    println(findMinimumSumOfPrimeSets(5))
     kotlin.system.exitProcess(0)
 }
 
 fun findMinimumSumOfPrimeSets(n: Int): Int {
-    val primeMap = mutableMapOf<Int, List<Int>>()
-    var tmpSum: Int
+    val primeChains = mutableMapOf<Int, List<List<Int>>>()
+    var tmpSum = 0
     var index = 0
-    while (true) {
+    while (index < primes.size) {
         val current = primes[index]
         val candidates = (0..<index).map { primes[it] }.filter { areConcatPrimes(current, it) }
-        primeMap[current] = candidates
-        candidates.map { candidate ->
-            val cands = primeMap[candidate]!!
-            cands.filter {
-                cands.any { cand -> primeMap[it]!!.contains(cand) }
-            }
+        val newChains = mutableListOf<List<Int>>()
+        candidates.forEach { prime ->
+            val chains = primeChains[prime]
+            if (chains.isNullOrEmpty()) {
+                newChains.add(listOf(prime, current))
+            } else {
+                chains.forEach { chain ->
+                    val newChain = chain.toMutableList()
+                    if (newChain.all { candidates.contains(it) }) {
+                        newChain.add(current)
+                        newChains.add(newChain)
+                        if (newChain.size == n) {
+                            println(newChain)
+                            tmpSum = newChain.sum()
+                        }
+                    }
+            }}
         }
-        var count = n - 1
-        val filtered = mutableListOf(current)
-        candidates.reversed().forEachIndexed { i, candidate ->
-            val remainings = candidates.subList(0, i)
-            if (remainings.count { e ->
-                    primeMap[e]?.contains(candidate) == true
-                } == count
-            ) {
-                filtered.add(candidate)
-                count--
-            }
-        }
-        if (filtered.size == n) {
-            tmpSum = filtered.sum()
-            println("$filtered")
-            println("tmpSum = $tmpSum")
-            break
-        }
+        primeChains[current] = newChains
         index++
-        continue
+        if (tmpSum != 0) break
     }
-    while (primes[index] < tmpSum) {
-        val current = primes[index]
+    while (primes[index-1] < tmpSum) {
+        val current = primes[index-1]
         val candidates = (0..<index).map { primes[it] }.filter { areConcatPrimes(current, it) }
-        primeMap[current] = candidates
-        var count = n - 1
-        val filtered = mutableListOf(current)
-        candidates.reversed().forEachIndexed { i, candidate ->
-            val remainings = candidates.subList(0, i)
-            if (remainings.count { e ->
-                    primeMap[e]?.contains(candidate) == true
-                } == count
-            ) {
-                filtered.add(candidate)
-                count--
+        val newChains = mutableListOf<List<Int>>()
+        candidates.forEach { prime ->
+            val chains = primeChains[prime]
+            if (chains.isNullOrEmpty()) {
+                newChains.add(listOf(prime, current))
+            } else {
+                chains.forEach { chain ->
+                    val newChain = chain.toMutableList()
+                    if (newChain.all { candidates.contains(it) }) {
+                        newChain.add(current)
+                        newChains.add(newChain)
+                        if (newChain.size == n) {
+                            println(newChain)
+                            tmpSum = min(tmpSum, newChain.sum())
+                        }
+                    }
+                }
             }
         }
-        if (filtered.size == n) {
-            tmpSum = min(filtered.sum(), tmpSum)
-            println("$filtered")
-            println("tmpSum = $tmpSum")
-        }
+        primeChains[current] = newChains
         index++
-        continue
     }
     return tmpSum
 }
